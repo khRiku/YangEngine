4f42988b5f90e6e3955cf63494559b6cc6c77a55
khRiku <1572883601@qq.com>
2019-08-05 08:33:07 +0800

    Update：新的资源模块编写(设计中）

---
+++ client/YangClient_u3d/Assets/Scripts/Module/AssetMoudule/ABCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+
+/// <summary>
+/// author: yxx
+/// date: 2019-8-5
+/// Descritption: 对AB和AB里的Asset 进行缓存和读取
+/// 
+/// </summary>
+public class ABCache
+{
+    private string mAbPath;
+    private Dictionary<string, Object> mAssetDic;
+
+    private void LoadAb()
+    {
+
+    }
+
+    private void LoadAbAsync()
+    {
+
+    }
+
+    public Object LoadAsset()
+    {
+        return null;
+    }
+
+    private void LoadAssetAsync(Action<Object> pCallback)
+    {
+
+    }
+}
+
